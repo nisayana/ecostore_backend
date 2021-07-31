bbceb302d149c0117f00464494c5c342ab5a2938
nisayana <nimaevasb@gmail.com>
2021-07-31 00:00:16 -0500

    some bugs

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,4 +1,4 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :username, :password, :first_name, :last_name, :current_booking, :past_bookings
+  attributes :id, :username, :password, :first_name, :last_name, :email, :address, :current_booking, :past_bookings
   # has_many :orders
 end
